5aab53d442ac409f2048b2d1b8e3267169003055
Jm7752 <48572733+Jm7752@users.noreply.github.com>
2021-04-06 19:54:29 -0500

    unlabeled

---
+++ train_unlabelled.py
@@ -0,0 +1,67 @@
+import os
+import argparse
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+from tqdm.notebook import tqdm
+import torch.optim as optim
+from utils.data_helper import CustomDataset
+import VAE
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--checkpoint_dir', default='checkpoint/', type=str)
+args = parser.parse_args()
+
+trainset = CustomDataset(root='/dataset', split="unlabelled", transform=train_transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=256, shuffle=True, num_workers=4)
+
+model = VAE.VAE(1600)
+epochs = 1
+lr = 1e-3
+momentum=0.9
+
+def loss_function(xhat, x, mu, logvar, kl_weight=1.0):
+    BCE = nn.functional.mse_loss(xhat, x, reduction='none').sum(1).mean()
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), 1).mean()
+
+    loss = BCE + kl_weight*KLD
+    return loss, BCE, KLD
+
+optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum) 
+model.train()
+if torch.cuda.is_available():
+    model = model.cuda()
+    device = torch.device("cuda:0")
+else:
+    device = torch.device("cpu")
+
+model.to(device)
+
+
+train_mse_loss_record = []
+
+for epoch in range(epochs):
+    total_train_loss = 0.0
+    for batch in tqdm(trainloader, leave=False):
+        x_in = batch[0].to(device=device)
+        x_label = batch[1].to(device=device)
+        outputs, mu, logvar = model(x_in)
+        loss, bce, kld = loss_function(outputs, x_label, mu, logvar, 0.5)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        total_train_loss += loss.item()
+
+    mean_train_loss = total_train_loss / len(trainset)
+
+    train_mse_loss_record.append(mean_train_loss)
+    
+    if epoch%10 ==0 :
+        os.makedirs(args.checkpoint_dir, exist_ok=True)
+        torch.save(net.state_dict(), os.path.join(args.checkpoint_dir, "net_demo.pth"))
+
+print('Finished Training')
+
+print(f"Saved checkpoint to {os.path.join(args.checkpoint_dir, 'net_demo.pth')}")
