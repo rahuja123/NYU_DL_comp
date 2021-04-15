92dffd1508736d29388621df2471c11390704485
Rahul Ahuja <rahulahuja_2k14@dtu.ac.in>
2021-04-15 16:51:50 +0000

    added repos back again

---
+++ barlowtwins/data_helper.py
@@ -0,0 +1,44 @@
+import os
+from PIL import Image
+
+import numpy as np
+import pandas as pd
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+
+# The dataset class for unlabeled data.
+
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, root, split, transform):
+        r"""
+        Args:
+            root: Location of the dataset folder, usually it is /dataset
+            split: The split you want to used, it should be one of train, val or unlabeled.
+            transform: the transform you want to applied to the images.
+        """
+        self.split = split
+        self.transform = transform
+
+        self.image_dir = os.path.join(root, split)
+        label_path = os.path.join(root, f"{split}_label_tensor.pt")
+
+        self.num_images = len(os.listdir(self.image_dir))
+
+        if os.path.exists(label_path):
+            self.labels = torch.load(label_path)
+        else:
+            self.labels = -1 * torch.ones(self.num_images, dtype=torch.long)
+
+    def __len__(self):
+        return self.num_images
+
+    def __getitem__(self, idx):
+        with open(os.path.join(self.image_dir, f"{idx}.png"), 'rb') as f:
+            img = Image.open(f).convert('RGB')
+
+        return self.transform(img), self.labels[idx]
+    
